7e5ecdbcfde4b31729813f4948588243c81d7843
Kristi Parrish <kp@Kristis-MacBook-Pro.local>
2019-12-06 10:33:46 -0700

    tabs created and rendering

--- components/Tabs/index.js
+++ components/Tabs/index.js
@@ -7,3 +7,28 @@
 //
 //  The tab component should look like this:
 //    <div class="tab">topic here</div>
+
+
+function tabMaker(topic){
+
+    const tabDiv = document.createElement('div');
+
+    tabDiv.classList.add('tab');
+
+    tabDiv.textContent = topic;
+
+    return tabDiv
+    }
+
+    const tabsEntryPoint = document.querySelector('.topics')
+
+    axios.get("https://lambda-times-backend.herokuapp.com/topics")
+    .then(response => {
+        console.log(response.data.topics);
+        response.data.topics.forEach(item => {
+            tabsEntryPoint.appendChild(tabMaker(item));
+        })
+    })
+    .catch(error => {
+        console.log("The data was not returned", error);
+    });
